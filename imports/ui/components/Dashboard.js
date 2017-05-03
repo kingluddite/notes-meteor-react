3da46cdc54c6b1b4018f8144f2042207027a9251
PEH2 <howley.phil@gmail.com>
2017-05-03 00:58:46 -0700

    Setup editor to change title/body

--- imports/ui/components/Dashboard.js
+++ imports/ui/components/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './Header';
 import NoteList from './NoteList';
+import Editor from './Editor';
 
 export default Dashboard = () => {
       return (
@@ -9,6 +10,7 @@ export default Dashboard = () => {
           <Header title="Dashboard" />
           <div className="page-content">
             <NoteList />
+            <Editor />
           </div>
         </div>
       );
